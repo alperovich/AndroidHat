c280900f4d823c5bc56c8e25e1b3f924a222dc97
Semen Alperovich <Semen.Alperovich@jetbrains.com>
2012-09-27 03:22:09 +0400

    dummy Logger, PlayersPool and WordsPool are added

---
+++ src/com/alperovichsimon/gamemodel/PlayersPool.java
@@ -0,0 +1,98 @@
+package com.alperovichsimon.gamemodel;
+
+import com.alperovichsimon.logger.Logger;
+
+import java.util.*;
+
+/**
+ * Simon Alperovich
+ * Date: 27.09.12
+ * Time: 2:30
+ */
+public class PlayersPool {
+    private static PlayersPool INSTANCE;
+
+    private Map<Player, Integer> guessedNumber = new LinkedHashMap<Player, Integer>();
+    private List<Player> pool = new ArrayList<Player>();
+
+
+    private PlayersPool() {
+
+    }
+
+    public static synchronized PlayersPool getInstance() {
+        if (INSTANCE == null){
+            INSTANCE  = new PlayersPool();
+        }
+        return INSTANCE;
+    }
+
+    public void addPlayer(Player player) {
+        pool.add(player);
+        guessedNumber.put(player, 0);
+    }
+
+    public List<Player> getPool(){
+        return pool;
+    }
+
+    public void playerGuessedWord(Player player) {
+        if (!guessedNumber.containsKey(player)) {
+            Logger.log("no such player: " + player + "in pool");
+        }
+        guessedNumber.put(player, guessedNumber.get(player) + 1);
+    }
+
+    public int  getNumberOfGuessedWords(Player player) {
+        if (!guessedNumber.containsKey(player)) {
+            Logger.log("no such player: " + player + "in pool");
+            return 0;
+        }
+        return guessedNumber.get(player);
+    }
+
+
+    private static class Player {
+        private Player partner;
+        private String name;
+
+        public Player() {
+
+        }
+
+
+        public Player(Player partner) {
+            if (partner.hasPartner()) {
+                Logger.log("Player " + partner + "already has partner" + partner.getPartner());
+            }
+
+            this.partner = partner;
+            partner.partner = this;
+
+        }
+
+        public boolean hasPartner() {
+            return partner != null;
+        }
+
+        public String toString() {
+            return name;
+        }
+
+        public void setPartner(Player partner) {
+            this.partner = partner;
+        }
+
+        public Player getPartner() {
+            return this.partner;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+    }
+}
---
+++ src/com/alperovichsimon/gamemodel/WordsPool.java
@@ -0,0 +1,61 @@
+package com.alperovichsimon.gamemodel;
+
+import com.alperovichsimon.logger.Logger;
+
+import java.util.*;
+
+/**
+ * Simon Alperovich
+ * Date: 27.09.12
+ * Time: 2:55
+ */
+public class WordsPool {
+    private static WordsPool INSTANCE;
+
+    private List<String> pool = new ArrayList<String>();
+    private Map<String, Boolean> guessed = new HashMap<String, Boolean>();
+    private List<Integer> indexes = new ArrayList<Integer>();
+    private int curIndex = 0;
+
+    private WordsPool() {
+
+    }
+
+    public void addWord(String word){
+        pool.add(word);
+        guessed.put(word, false);
+        indexes.add(pool.size());
+    }
+
+    public void wordGuessed(String word){
+        if (!guessed.containsKey(word)){
+            Logger.log("No such word: "+ word);
+        }
+        guessed.put(word, true);
+    }
+
+    public void shuffle(){
+        Collections.shuffle(indexes);
+        curIndex = 0;
+    }
+
+    public String getNextWord(){
+        int size = pool.size();
+        while (!guessed.get(pool.get(indexes.get(curIndex))) && curIndex < size){
+            ++curIndex;
+        }
+        if (curIndex >= size){
+            return null;
+        }
+        return pool.get(indexes.get(curIndex));
+    }
+
+
+
+    public static synchronized WordsPool getInstance() {
+        if (INSTANCE == null){
+            INSTANCE = new WordsPool();
+        }
+        return INSTANCE;
+    }
+}
