8fce880d6346ac8698c44116997c0f3ba714def1
Semen Alperovich <Semen.Alperovich@jetbrains.com>
2012-09-26 04:06:57 +0400

    relative layout->linear layout newer interface

--- gen/com/alperovichsimon/R.java
+++ gen/com/alperovichsimon/R.java
@@ -14,19 +14,23 @@ public final class R {
         public static final int main_screen=0x7f020000;
     }
     public static final class id {
-        public static final int player_counter_text=0x7f050003;
-        public static final int prepare_button=0x7f050001;
-        public static final int start_button=0x7f050000;
-        public static final int word_counter_text=0x7f050002;
+        public static final int edit_players_button=0x7f050003;
+        public static final int edit_words_button=0x7f050002;
+        public static final int player_counter_text=0x7f050005;
+        public static final int start_button=0x7f050001;
+        public static final int title=0x7f050000;
+        public static final int word_counter_text=0x7f050004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int player_counter_text=0x7f040004;
-        public static final int prepare_button_text=0x7f040002;
-        public static final int start_button_text=0x7f040001;
-        public static final int word_counter_text=0x7f040003;
+        public static final int edit_players_button_text=0x7f040004;
+        public static final int edit_words_button_text=0x7f040003;
+        public static final int player_counter_text=0x7f040006;
+        public static final int start_button_text=0x7f040002;
+        public static final int title_text=0x7f040001;
+        public static final int word_counter_text=0x7f040005;
     }
 }
