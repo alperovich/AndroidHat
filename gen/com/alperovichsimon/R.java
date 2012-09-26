12d45396266a105b55e72528f8b9af412b98434f
Semen Alperovich <Semen.Alperovich@jetbrains.com>
2012-09-26 04:42:41 +0400

    add icon

--- gen/com/alperovichsimon/R.java
+++ gen/com/alperovichsimon/R.java
@@ -11,7 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int main_screen=0x7f020000;
+        public static final int ic_icon=0x7f020000;
+        public static final int main_screen=0x7f020001;
     }
     public static final class id {
         public static final int edit_players_button=0x7f050003;
